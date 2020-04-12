fbbeb00671da5545189210a358bdd51f867e0f6e
tuttlepower <tatuttle2015@gmail.com>
2020-04-12 13:12:19 -0400

    17,18,19,20

--- scripts/OSUDatabaseCreator.py
+++ scripts/OSUDatabaseCreator.py
@@ -1,16 +1,11 @@
 # import pandas as pd
 import pandas as pd
-
 #currently at 14 as of 4/6/2020
-
 #adds additional day 14 will be next
-day = 16
-
+day = 20
 temp = pd.read_json (r'https://collegefootballrisk.com/api/team/players?season=2&day='+str(day)+'&team=ohio%20state')
-
 #creates df, and appends new data to it
 #df = df.append(temp, ignore_index = True)
-
 #outputs new data to csv
 temp.to_csv(r'data/osudata.csv', index = False,mode='a', header=False)
 
